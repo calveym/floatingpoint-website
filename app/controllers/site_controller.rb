55d395ae94471b09a2cde334154909fd2523d04f
LMCMLJ <mjcalvey2@gmail.com>
2017-01-14 14:46:22 +0000

    Creates basic rails starter app

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,16 @@
+class SiteController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def presskit
+  end
+
+  def blog
+  end
+
+  def team
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'site/home'
+
+  get 'site/contact'
+
+  get 'site/presskit'
+
+  get 'site/blog'
+
+  get 'site/team'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
