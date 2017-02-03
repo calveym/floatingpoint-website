30b86aeac005961ec2c8aed2f2606b31a57e81b5
Royston Henson <rlqhenson@hotmail.com>
2017-02-03 14:24:25 +0000

    routed page link hrefs

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'site/home' => 'site#home'
 
+  get 'site/home' => 'site#home'
+  get 'site/team', to: 'site#team'
+  get 'site/presskit', to: 'site#presskit'
+  get 'site/blog', to: 'site#blog'
   get 'site/contact', to: 'site#contact'
 
-  get 'site/presskit', to: 'site#presskit'
-
-  get 'site/blog', to: 'site#blog'
-
-  get 'site/team', to: 'site#team'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
