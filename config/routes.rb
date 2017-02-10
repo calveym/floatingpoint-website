5ea5fe5b813125a4a0f001df608d8f07014b88c2
Royston Henson <rlqhenson@hotmail.com>
2017-02-10 19:09:03 +0000

    made homepage root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'site/home' => 'site#home'
+  root to: 'site#home'
   get 'site/team', to: 'site#team'
   get 'site/presskit', to: 'site#presskit'
   mount Monologue::Engine, at: '/site/blog'
