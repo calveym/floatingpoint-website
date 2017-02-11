302aa1503cf8b0ad77ca2b2af2f1e741ad866f27
Royston Henson <rlqhenson@hotmail.com>
2017-02-11 12:36:03 +0000

    updated page routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   root to: 'site#home'
-  get 'site/team', to: 'site#team'
-  get 'site/presskit', to: 'site#presskit'
-  mount Monologue::Engine, at: '/site/blog'
-  get 'site/contact', to: 'site#contact'
+  get '/team', to: 'site#team'
+  get '/presskit', to: 'site#presskit'
+  mount Monologue::Engine, at: '/blog'
+  get '/contact', to: 'site#contact'
 
 end
